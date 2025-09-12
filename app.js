019fa54ccd0ebf359316be7321b0f839b6eaa51d
Takudzwa88 <kanokangatakudzwa@gmail.com>
2025-09-12 09:16:41 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,37 @@
+//console.log('hey ninjas');
+
+/*
+var time = 0;
+var timer = setInterval(function (){
+    time += 2;
+    console.log(time + ' seconds have passed');
+    if (time > 5){
+        clearInterval(timer);
+    }
+}, 2000);
+*/
+
+/*
+console.log(__dirname);
+console.log(__filename);
+*/
+
+//normal function statement
+/*
+function sayHi(){
+    console.log('hi');
+}
+
+sayHi();
+*/
+
+function callFunction(fun){
+    fun();
+}
+
+//function expression
+var sayBye = function(){
+    console.log('bye');
+};
+
+callFunction(sayBye);
