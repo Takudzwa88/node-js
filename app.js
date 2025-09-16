a3f9a98be690569d5f1527a9d8e06b9b45f354af
Takudzwa88 <kanokangatakudzwa@gmail.com>
2025-09-16 15:38:15 +0200

    added new code

--- app.js
+++ app.js
@@ -47,7 +47,31 @@ console.log(stuff.adder(5, 6));
 console.log(stuff.adder(stuff.pi, 5));
 */
 
+/*
 var events = require('events');
+var util = require('util');
+
+var Person = function(name){
+    this.name = name;
+};
+
+util.inherits(Person, events.EventEmitter);
+
+var james = new Person('james');
+var mary = new Person('mary');
+var ryu = new Person('ryu');
+var people = [james, mary, ryu];
+
+people.forEach(function(person){
+    person.on('speak', function(mssg){
+        console.log(person.name + ' said: ' + mssg);
+        });
+    });
+
+
+james.emit('speak', 'hey dudes');
+ryu.emit('speak', 'I want a curry!');
+*/
 
 /*
 var myEmitter = new events.EventEmitter();
@@ -58,3 +82,36 @@ myEmitter.on('someEvent', function(mssg){
 
 myEmitter.emit('someEvent', 'the event was emitted!');
 */
+
+var fs = require('fs');
+
+fs.readFile('readMe.txt', 'utf8', function(err, data){
+    if (err) {
+        console.error('Error reading file:', err);
+        return;
+    }
+
+    fs.writeFile('writeMe.txt', data, function(err) {
+        if (err) {
+            console.error('Error writing file:', err);
+        } else {
+            console.log('File written successfully!');
+        }
+    });
+});
+
+
+/*
+fs.readFile('readMe.txt', 'utf8', function(err, data){
+    fs.writeFile('writeMe.txt', data);
+  //  console.log(data);
+}); //non-blocking code
+*/
+//fs.writeFileSync('writeMe.txt', readMe); //we use writeFileSync to make sure the file is written only after readFile is done
+//console.log(readMe);  //to print stuff from readMe.txt
+
+//console.log('test');  //this will print before readMe.txt because readFile is non-blocking
+
+//code
+
+
