ad1bb9c95b83328bbc9d50599ec6c55002205d9e
Takudzwa88 <kanokangatakudzwa@gmail.com>
2025-09-23 12:04:11 +0200

    added new code

--- app.js
+++ app.js
@@ -212,15 +212,18 @@ myReadStream.on('data', function(chunk){ // when we get some data
 var express = require('express');
 var app = express();
 
+app.set('view engine', 'ejs');
+
 app.get('/', function(req, res){
-    res.send('This is the homepage');
+    res.sendFile(__dirname + '/index.html');
 });
 app.get('/contact', function(req, res){
-    res.send('This is the contact page');
+    res.sendFile(__dirname + '/contact.html');
 });
 
 app.get('/profile/:name', function(req, res){
-    res.send('You requested to see a profile with the name of ' + req.params.name);
+    var data = {age: 29, job: 'ninja'};
+    res.render('profile', {person: req.params.name, data: data});
 });
 
 app.listen(3000);
